3701dae91b37375b2f63ed4c131f9d27b94aecdd
Jude Seruch <judeseruch@gmail.com>
2024-11-26 10:55:25 +0100

    capture sound add

--- src/models/assets/index.py
+++ src/models/assets/index.py
@@ -3,7 +3,7 @@ from utils.const import ASSETS_DIR
 class Assets:
 
       audio_game_completed = ASSETS_DIR / "audios/game_completed.wav"
-      audio_take_pawn = ASSETS_DIR / "audios/take_pawn.wav"
+      audio_take_pawn = ASSETS_DIR / "audios/game-gun-shot-1662_4RV1AJeJ.wav"
       audio_background = ASSETS_DIR / "audios/bg.wav"
       
       img_red_soldier = ASSETS_DIR / "images/red_soldier.png"
@@ -19,8 +19,6 @@ class Assets:
 
       cible = ASSETS_DIR / "images/cible.png"
       approuve = ASSETS_DIR / "images/approuve.png"
-
-      
 
       dir_avatar = ASSETS_DIR / "avatars"
       
