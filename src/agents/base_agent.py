a1f7ea0d7a945c2227fd7dfb585c2b0091632f14
elonmj <josaphatahouanye@gmail.com>
2024-12-16 13:46:39 +0100

    feat: pass an image for ai in folder agents/avatars

--- src/agents/base_agent.py
+++ src/agents/base_agent.py
@@ -1,18 +1,19 @@
 import random
 import os
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 import sys
 from typing import List, Dict, Literal
 from src.models.assets.index import Assets
-from src.utils.const import Soldier
+from src.utils.const import AGENT_AVATAR_DIR, Soldier
 
 @dataclass
 class MatchPerformance:
     issue: Literal["win", "loss", "draw"]
+    margin: int
     number_of_moves: int
     time: float
     opponent: str
-    reason: str  # Added 'reason' attribute
+    reason: str 
 
 
 class BaseAgent:
@@ -28,32 +29,41 @@ class BaseAgent:
             self.performances = [MatchPerformance(**performance) for performance in data.get("performances", [])]
             self.profile_img = data.get("profile_img", "")
         else:
-            self.performances = []
+            self.performances = []       
+            
             self.profile_img = self._get_random_avatar()
+            
+            extensions = ['.png', '.jpg', '.jpeg']
+            for ext in extensions:
+                if os.path.exists(os.path.join(AGENT_AVATAR_DIR, self.pseudo + ext)):
+                    self.profile_img = os.path.join(AGENT_AVATAR_DIR, self.pseudo + ext)
+                    break
+        
+            
 
     
     def _get_random_avatar(self) -> str:
         """Gets a random avatar path from assets"""
         avatar_dir = Assets.dir_avatar
-        # print("avatar",avatar_dir)
+        
         avatar_files = [f for f in os.listdir(avatar_dir) 
                        if f.endswith(('.png', '.jpg', '.jpeg'))]
         
-        # print("avatar",avatar_files)
         if avatar_files:
             return os.path.join(avatar_dir, random.choice(avatar_files))
         return ""  
     
 
-    def conclude_game(self, issue : Literal['win', 'loss', 'draw'], opponent_name: str, number_of_moves : int, time : float, reason: str) -> None:
+    def conclude_game(self, issue : Literal['win', 'loss', 'draw'],  opponent_name: str, number_of_moves : int, time : float, reason: str, margin:int = 16) -> None:
         """Updates agent statistics after game conclusion"""
 
         performance = MatchPerformance(
             issue=issue,
+            margin= margin,
             number_of_moves= number_of_moves,
             time=   time,
             opponent=opponent_name,
-            reason=reason  # Include 'reason' when creating MatchPerformance
+            reason=reason  
         )
         self.performances.append(performance)
         
